afccb242fd85ac406b34deef7c9c60d6661d9d27
jonaugustine <jon.augustine@dfltechnology.com>
2022-01-15 13:23:06 -0500

    added initial easy install script

---
+++ scripts/easy_install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -x
+
+# make sure you have Git installed
+echo "Downloading package information from configured sources..."
+sudo apt -y update
+
+echo "Making sure you have Git installed..."
+sudo apt -y install git
+
+echo "Saving current working directory..."
+pushd .
+
+echo "Going into your home directory..."
+cd ~
+
+echo "Cloning the FreeTAKHub-Installation repository..."
+git clone https://github.com/FreeTAKTeam/FreeTAKHub-Installation.git
+
+echo "Going into the FreeTAKHub-Installation directory..."
+cd FreeTAKHub-Installation
+
+echo "Run the initialization script..."
+./init.sh
+
+echo "Run the installation script..."
+./install.sh
+
+echo "Going back to your original working directory..."
+set +x
