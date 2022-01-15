6362ac933560a2ca9072a632c293f4bd9f6bb3ca
jonaugustine <95979847+jonaugustine@users.noreply.github.com>
2022-01-15 18:33:34 -0500

    Update setup-Ansible.sh

--- scripts/setup-Ansible.sh
+++ scripts/setup-Ansible.sh
@@ -7,7 +7,7 @@ sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt -y update
 sudo apt -y install ansible
 
-echo: "Add passwordless Terraform and Ansible execution for the current user"
+echo "Add passwordless Terraform and Ansible execution for the current user"
 # only add if non-existent
 LINE="$USER ALL=(ALL) NOPASSWD:/usr/bin/ansible-playbook,/usr/bin/terraform"
 FILE="/etc/sudoers.d/dont-prompt-$USER-for-sudo-password"
