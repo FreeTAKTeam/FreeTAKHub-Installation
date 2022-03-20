23e6f9d549ff249df2d13baf42f982dc3cc6d598
jonaugustine <95979847+jonaugustine@users.noreply.github.com>
2022-03-20 09:23:45 -0700

    make shell linter happy

--- scripts/easy_install.sh
+++ scripts/easy_install.sh
@@ -50,9 +50,23 @@ cleanup() {
 ###############################################################################
 function setup_colors() {
   if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
-    NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
+    NOFORMAT='\033[0m'
+    RED='\033[0;31m'
+    GREEN='\033[0;32m'
+    # ORANGE='\033[0;33m' # unused
+    BLUE='\033[0;34m'
+    # PURPLE='\033[0;35m' # unused
+    # CYAN='\033[0;36m' # unused
+    YELLOW='\033[1;33m'
   else
-    NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
+    NOFORMAT=''
+    RED=''
+    GREEN=''
+    # ORANGE='' # unused
+    BLUE=''
+    # PURPLE='' # unused
+    # CYAN='' # unused
+    YELLOW=''
   fi
 }
 
