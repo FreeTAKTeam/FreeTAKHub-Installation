6d21d8752e446c5bcf449467d277d4c80cab6463
Fredrick Eisele <fred.eisele@vanderbilt.edu>
2024-10-08 16:14:59 -0500

    must install venv with python3-venv

--- scripts/easy_install.sh
+++ scripts/easy_install.sh
@@ -605,7 +605,7 @@ function download_dependencies() {
 ###############################################################################
 function install_python_environment() {
   apt-get update
-  apt-get install -y python3-pip python3-setuptools
+  apt-get install -y python3-pip python3-setuptools python3-venv
   apt-get install -y python${PY3_VER}-dev python${PY3_VER}-venv libpython${PY3_VER}-dev
 
   /usr/bin/python${PY3_VER} -m venv ${FTS_VENV}
